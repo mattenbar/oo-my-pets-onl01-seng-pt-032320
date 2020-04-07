2e27ba1df84edeafc63d3810b16a9f06ced05f92
matthew enbar <matthew.enbar@gmail.com>
2020-04-07 22:06:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,23 @@
 require "pry"
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize (name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
 end
