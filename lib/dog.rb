a51430a7ae4e5f6f0f3e622f6d39a2480b8d2665
matthew enbar <matthew.enbar@gmail.com>
2020-04-07 23:37:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
+require "pry"
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+  @@count = 0
+
+  def initialize (name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+    @@count +=1
+  end
+
+  def self.reset_all
+    @@all.clear
+    @@count = 0
+  end
+
 end
