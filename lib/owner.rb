f521968f10ea60903023b364b7ad98556c9769f0
matthew enbar <matthew.enbar@gmail.com>
2020-04-07 21:13:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,11 @@
 require "pry"
 class Owner
   # code goes here
-  attr_reader :name
+  attr_reader :name, :species
 
   def initialize (name)
     @name = name
+    @species = "human"
   end
 
 end
