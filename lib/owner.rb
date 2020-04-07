5d9a21600eb4271ab189f3d7d4d8d08971a14022
matthew enbar <matthew.enbar@gmail.com>
2020-04-07 23:36:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,7 +70,7 @@ class Owner
   end
 
   def list_pets
-    puts "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
+    return "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
   end
 
 end
